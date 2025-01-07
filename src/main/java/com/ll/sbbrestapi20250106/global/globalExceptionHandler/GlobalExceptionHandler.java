babfae55b998110e54935e821b9fa0e28b60a10b
sungyeong98 <am25227903@gmail.com>
2025-01-07 11:40:23 +0900

    feat: 회원 ID 중복 처리

--- src/main/java/com/ll/sbbrestapi20250106/global/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/ll/sbbrestapi20250106/global/globalExceptionHandler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.ll.sbbrestapi20250106.global.globalExceptionHandler;
 import com.ll.sbbrestapi20250106.global.app.AppConfig;
 import com.ll.sbbrestapi20250106.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -49,5 +50,15 @@ public class GlobalExceptionHandler {
                 ));
     }
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<RsData<Void>> handle(DataIntegrityViolationException ex) {
+        if (AppConfig.isNotProd()) ex.printStackTrace();
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new RsData<>(
+                        "400-1",
+                        "이미 존재하는 데이터 입니다."
+                ));
+    }
 
 }
