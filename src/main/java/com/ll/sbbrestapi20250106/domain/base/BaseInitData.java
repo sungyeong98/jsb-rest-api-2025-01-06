6d1ac4fdb27936fc71595f35354cc2ba5c2fbbc5
sungyeong98 <am25227903@gmail.com>
2025-01-07 12:50:17 +0900

    feat: 샘플데이터 생성

--- src/main/java/com/ll/sbbrestapi20250106/domain/base/BaseInitData.java
+++ src/main/java/com/ll/sbbrestapi20250106/domain/base/BaseInitData.java
@@ -2,7 +2,7 @@ package com.ll.sbbrestapi20250106.domain.base;
 
 import com.ll.sbbrestapi20250106.domain.question.Question;
 import com.ll.sbbrestapi20250106.domain.question.QuestionService;
-import com.ll.sbbrestapi20250106.domain.user.User;
+import com.ll.sbbrestapi20250106.domain.user.SiteUser;
 import com.ll.sbbrestapi20250106.domain.user.UserService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -35,19 +35,22 @@ public class BaseInitData {
     public void work1() {
         if (userService.count() > 0) return;
 
-        User admin = userService.join("admin", "123", "admin", "admin@admin.com");
-        User user1 = userService.join("user1", "123", "user1", "user1@user.com");
-        User user2 = userService.join("user2", "123", "user2", "user2@user.com");
-        User user3 = userService.join("user3", "123", "user3", "user3@user.com");
+        SiteUser admin = userService.join("admin", "123", "admin", "admin@admin.com");
+        SiteUser user1 = userService.join("user1", "123", "user1", "user1@user.com");
+        SiteUser user2 = userService.join("user2", "123", "user2", "user2@user.com");
+        SiteUser user3 = userService.join("user3", "123", "user3", "user3@user.com");
     }
 
     @Transactional
     public void work2() {
         if (questionService.count() > 0) return;
 
-        Question q1 = questionService.write("test1", "1111111");
-        Question q2 = questionService.write("test2", "2222222");
-        Question q3 = questionService.write("test3", "3333333");
+        SiteUser user1 = userService.findByUsername("user1").get();
+        SiteUser user2 = userService.findByUsername("user2").get();
+
+        Question q1 = questionService.write("test1", "1111111", user1);
+        Question q2 = questionService.write("test2", "2222222", user2);
+        Question q3 = questionService.write("test3", "3333333", user2);
 
     }
 
