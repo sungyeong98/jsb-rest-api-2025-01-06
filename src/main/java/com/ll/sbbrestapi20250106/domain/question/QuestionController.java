d5b727f4936d9c9d8b04d7d0e50b367e323b5216
sungyeong98 <am25227903@gmail.com>
2025-01-07 09:27:24 +0900

    feat: RsData 클래스 생성 및 적용

--- src/main/java/com/ll/sbbrestapi20250106/domain/question/QuestionController.java
+++ src/main/java/com/ll/sbbrestapi20250106/domain/question/QuestionController.java
@@ -1,5 +1,6 @@
 package com.ll.sbbrestapi20250106.domain.question;
 
+import com.ll.sbbrestapi20250106.global.rsData.RsData;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -28,17 +29,14 @@ public class QuestionController {
     }
 
     @DeleteMapping("/{id}")
-    public Map<String, Object> deleteQuestion(@PathVariable Long id) {
+    public RsData deleteQuestion(@PathVariable Long id) {
         Question question = questionService.findById(id).get();
         questionService.delete(question);
 
-        // rsData는 자세한 응답 내용을 담기 위한 존재
-        // 이걸 안쓸거면 그냥 문자열만 반환해도 될 듯함 (이걸 꼭 사용해야 되는 건 아닌듯)
-        Map<String, Object> rsData = new HashMap<>();
-        rsData.put("resultCode", "200-1");
-        rsData.put("msg", "%d번 게시물이 삭제되었습니다.".formatted(id));
-
-        return rsData;
+        return new RsData(
+                "200-1",
+                "%번 글이 삭제되었습니다.".formatted(id)
+        );
     }
 
     @AllArgsConstructor
@@ -50,17 +48,16 @@ public class QuestionController {
 
     @PutMapping("/{id}")
     @Transactional
-    public Map<String, Object> modifyQuestion(@PathVariable Long id,
+    public RsData modifyQuestion(@PathVariable Long id,
                                               @RequestBody QuestionModifyReqBody reqBody) {
         Question question = questionService.findById(id).get();
 
         questionService.modify(question, reqBody.getSubject(), reqBody.getContent());
 
-        Map<String, Object> rsData = new HashMap<>();
-        rsData.put("resultCode", "200-1");
-        rsData.put("msg", "%d번 글이 수정되었습니다.".formatted(id));
-
-        return rsData;
+        return new RsData(
+                "200-1",
+                "%d번 글이 수정되었습니다.".formatted(id)
+        );
     }
 
 }
---
+++ src/main/java/com/ll/sbbrestapi20250106/global/rsData/RsData.java
@@ -0,0 +1,11 @@
+package com.ll.sbbrestapi20250106.global.rsData;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class RsData {
+    private String resultCode;
+    private String msg;
+}
