3be8dd76f992c9b930600bd4d4427b888499ac91
sungyeong98 <am25227903@gmail.com>
2025-01-08 11:34:01 +0900

    feat: jwt-9강 완료

--- src/main/java/com/ll/sbbrestapi20250106/standard/util/Ut.java
+++ src/main/java/com/ll/sbbrestapi20250106/standard/util/Ut.java
@@ -1,7 +1,16 @@
 package com.ll.sbbrestapi20250106.standard.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ClaimsBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.SneakyThrows;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 
 public class Ut {
 
@@ -17,6 +26,26 @@ public class Ut {
         @SneakyThrows
         public static String toString(Object obj) {
             return om.writeValueAsString(obj);
+        }
+    }
+
+    public static class jwt {
+        public static String toString(String secret, int expireSeconds, Map<String, Object> body) {
+            ClaimsBuilder claimsBuilder = Jwts.claims();
+            for (Map.Entry<String, Object> entry : body.entrySet()) {
+                claimsBuilder.add(entry.getKey(), entry.getValue());
+            }
+            Claims claims = claimsBuilder.build();
+            Date issuedAt = new Date();
+            Date expiration = new Date(issuedAt.getTime() + 1000L * expireSeconds);
+            Key secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+            String jwt = Jwts.builder()
+                    .setClaims(claims)
+                    .setIssuedAt(issuedAt)
+                    .setExpiration(expiration)
+                    .signWith(secretKey, SignatureAlgorithm.HS256)
+                    .compact();
+            return jwt;
         }
     }
 }
