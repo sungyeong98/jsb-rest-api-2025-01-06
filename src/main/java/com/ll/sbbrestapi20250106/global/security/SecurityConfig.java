72a32a0b3d3b3f6d60ea72a5b5360d7628551dee
sungyeong98 <am25227903@gmail.com>
2025-01-08 16:48:56 +0900

    feat: jwt-37강 완료

--- src/main/java/com/ll/sbbrestapi20250106/global/security/SecurityConfig.java
+++ src/main/java/com/ll/sbbrestapi20250106/global/security/SecurityConfig.java
@@ -36,27 +36,30 @@ public class SecurityConfig {
                 .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(exceptionHandling -> exceptionHandling
                         .authenticationEntryPoint((request, response, authException) -> {
-                            response.setContentType("application/json; charset=utf-8");
+                                    response.setContentType("application/json; charset=utf-8");
 
-                            boolean is401 = authException.getLocalizedMessage().contains("authentication is required");
+                                    response.setStatus(401);
+                                    response.getWriter().write(
+                                            Ut.json.toString(
+                                                    new RsData("401-1", "사용자의 인증정보가 올바르지 않습니다.")
+                                            )
+                                    );
+                                }
+                        )
+                        .accessDeniedHandler(
+                                ((request, response, accessDeniedException) -> {
+                                    response.setContentType("application/json; charset=utf-8");
 
-                            if (is401) {
-                                response.setStatus(401);
-                                response.getWriter().write(
-                                        Ut.json.toString(
-                                                new RsData("401-1", "사용자 인증정보가 올바르지 않습니다.")
-                                        )
-                                );
-                                return;
-                            }
-
-                            response.setStatus(403);
-                            response.getWriter().write(
-                                    Ut.json.toString(
-                                            new RsData("403-1", request.getRequestURI() + ", " + authException.getLocalizedMessage())
-                                    )
-                            );
-                        }))
+                                    response.setStatus(403);
+                                    response.getWriter().write(
+                                            Ut.json.toString(
+                                                    new RsData("403-1", "접근 권한이 없습니다.")
+                                            )
+                                    );
+                                }
+                                )
+                        )
+                )
         ;
 
         return http.build();
